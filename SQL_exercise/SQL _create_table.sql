fbdf8501d03eb68a7ad06c00b17fefa2d4e55347
hanyeh00 <44142415+hanyeh00@users.noreply.github.com>
2021-02-19 11:38:48 +0330

    Add files via upload

---
+++ SQL_exercise/SQL _create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cities(name VARCHAR(50), country VARCHAR(50), population INT, area INT) 
+
+INSERT INTO cities(name, country, population, area)
+VALUES('Tokyo','Japan', 37274000, 13452),
+('Delhi',	 'India',	28514000,	1484),
+('Shanghai'	, 'China', 24183000, 6341)	,
+('Sao paulo','Brazil',12252000,1521),
+('Mexico City',	 'Mexico', 8918653,1485);
