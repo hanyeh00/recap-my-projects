76b23a07f03a1c4810006a9564c6640044d76117
hanyeh00 <44142415+hanyeh00@users.noreply.github.com>
2021-03-22 15:04:04 +0430

    Create Person.cs

---
+++ Basic_class/class/Person.cs
@@ -0,0 +1,18 @@
+using systems;
+namespace 
+{
+public class Person
+{
+       // public class is accesable any where in applications.
+       public string Name; //feild
+       public void Introduce()
+       
+       {    Console.WriteLine("My name is :" , Name);
+
+
+        }
+
+
+}
+
+}
