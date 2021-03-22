57f432acd545b7738f299d4576ef74615295895f
hanyeh00 <44142415+hanyeh00@users.noreply.github.com>
2021-03-22 15:02:27 +0430

    Create main.cs
    classes combine related variables(fields), and functions(method);
    from class, we can create objects
    objects are instances of class.

---
+++ Basic_class/class/main.cs
@@ -0,0 +1,20 @@
+using system;
+namespace person 
+{
+
+public class Program
+{
+
+// create objects
+     static void main(string[] , args)
+{
+    int number;
+    var myclass= new Person();
+    myclass.Name="Bahar";
+    myclass. Introduce();
+   }
+   }
+   // we cannot access to static function by creating object....
+   // ctr+f5
+   //alt+ enter= resharper
+   }
