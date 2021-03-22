0969886ee1cda5da656afb61addb8dccf187a06f
hanyeh00 <44142415+hanyeh00@users.noreply.github.com>
2021-03-22 19:47:00 +0430

    Update and rename Basic_class/conditonal.cs to Basic_class/conditonal/Season.cs

---
+++ Basic_class/conditonal/Season.cs
@@ -0,0 +1,15 @@
+
+// if condition is true go to the block
+// else if non of the if conditions is true this block is execute
+using system;
+namespace conditional
+{     
+   public enum Season
+   {
+       Spring, summer, Automn, Winter
+      
+   }
+
+
+
+}
