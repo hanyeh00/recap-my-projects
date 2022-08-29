7c70e0bea1fa0f92bc1304cc572852e2ab7b78cf
hanieh <h.khaleoghli@gmail.com>
2022-08-29 12:37:28 +0430

    Update 6Primery_Key.sql

--- SQL_exercise/6Primery_Key.sql
+++ SQL_exercise/6Primery_Key.sql
@@ -6,7 +6,54 @@ age int,
 city varchar(50));
 
 
+create table products
+(Id int Primary Key identity(1,1),
+ProducttName varchar(50)
+price float);
+
+create table Orders
+(OrderId int Primary Key identity(1,1),
+CustomerId int,
+Orderdate Datetime,
+ProductId int)
+
+
+
+-- vales of table
+
+
+
 insert into customert
 (FirstName,LastName,Age,city)
 values('shervin','hamidi','41','Tehran')
 
+insert into products
+(ProducttName, price)
+values('ball',2.95)
+insert into products
+(ProducttName, price)
+values('pen',10.75)
+insert into products
+(ProducttName, price)
+values('paper',3)
+insert into products
+(ProducttName, price)
+values('notebook',4)
+
+
+
+insert into Orders
+(Orderdate,CustomerId,ProductId)
+values(GETDATE(), 2,2);
+
+--alter
+
+alter table Orders
+add foreign key (customerId)
+      references customert(Id)
+      
+alter table Orders
+add foreign key (ProductId)
+      references Products(Id)
+      
+       
