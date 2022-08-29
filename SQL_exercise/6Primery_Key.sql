055160e2e5e268e628adb919420b2d9f990e718a
hanieh <h.khaleoghli@gmail.com>
2022-08-29 11:54:30 +0430

    Create 6Primery_Key.sql

---
+++ SQL_exercise/6Primery_Key.sql
@@ -0,0 +1,12 @@
+create table Customer
+(id int Primary Key identity (1,1),
+FirstName varchar(50),
+LastName= varchar(50),
+age int,
+city varchar(50));
+
+
+insert into customert
+(FirstName,LastName,Age,city)
+values('shervin','hamidi','41','Tehran')
+
